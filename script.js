a369ba0ce33cfe93b15ffafb0f8e3598d97230df
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-08-21 22:25:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function getFormvalue() {
     //Write your code here
+	function getFormvalue() {
+    // Get the value of the first name field
+    var firstName = document.getElementById('fname').value;
 
+    // Get the value of the last name field
+    var lastName = document.getElementById('lname').value;
+
+    // Display an alert with the full name
+    alert(firstName + " " + lastName);
+}
+
+	
 }
